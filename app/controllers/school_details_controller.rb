c4385d0630484fd69076497fe3cf1dc078480446
dharmik897 <dharmiksalakiya8.ds@gmail.com>
2021-02-25 15:15:23 +0530

    generate charts and compare reports

--- app/controllers/school_details_controller.rb
+++ app/controllers/school_details_controller.rb
@@ -4,4 +4,13 @@ class SchoolDetailsController < ApplicationController
   def index
     @schools = SchoolDetail.all
   end
+
+  def year_and_school
+    @school = SchoolDetail.find_by(id: params[:id])
+  end
+
+  def compare_schools
+    @school1 = SchoolDetail.find_by(id: params[:school_one])
+    @school2 = SchoolDetail.find_by(id: params[:school_two])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'school_details#index'
+  resources :school_details do
+    get 'year_and_school'
+    get 'compare_schools'
+  end
   namespace :api do
     namespace :v1 do
       resources :matrics_details, only: %i[index new show create]
